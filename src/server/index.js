f35152d2be4b13527e942a5a416fc4291b7fe6fa
Victorino Lavida <sloker@pop-os.localdomain>
2022-05-07 10:13:15 -0500

    controller para obtener y crear mensaje

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import dbConnection from './database/config';
 import router from './routes/auth';
+import routerMessages from './routes/message';
 dotenv.config();
 
 dbConnection();
@@ -12,7 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', router);
+app.use('/api', [router,routerMessages]);
 
 app.listen(4000, () => {
   console.log(`servidor corriendo en 4000`);
